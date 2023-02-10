bc22c44f7baccc46aa5d70a49debd2e58e9138fd
Shubham Prasad <61153966+SAMURAii-7@users.noreply.github.com>
2023-02-10 19:41:43 +0530

    update TestController

--- src/main/java/com/shubham/nmsbackend/controllers/TestController.java
+++ src/main/java/com/shubham/nmsbackend/controllers/TestController.java
@@ -8,16 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api/test")
+@RequestMapping("/api")
 public class TestController {
-    @GetMapping("/all")
-    public String allAccess() {
-        return "Public Content";
-    }
-
-    @GetMapping("/user")
-    @PreAuthorize("hasRole('USER')")
-    public String userAccess() {
-        return "User Content";
+    @GetMapping("/")
+    public String hello() {
+        return "Welcome to NMS API!";
     }
 }
